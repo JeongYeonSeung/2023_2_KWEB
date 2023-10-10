8b0d693db8a07ac6a2b49e5389c3fc3f979f113a
JeongYeonSeung <asas4654@daum.net>
2023-10-10 18:46:38 +0900

    HW2.2

--- HW/Chapter2/Exercise2/index.2.2.js
+++ HW/Chapter2/Exercise2/index.2.2.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 
-const getDirectory = util.promisify(fs.readdir);
-const getFileStat = util.promisify(fs.stat);
+const readdir = util.promisify(fs.readdir);
+const stat = util.promisify(fs.stat);
 
 /** 현재 작업 디렉토리를 스크립트 파일의 디렉토리로 변경 */
 process.chdir(__dirname);  
@@ -11,11 +11,11 @@ const testPath = "./test";
 
 /** path를 받아서 search */
 const searchDirectory = async (directory) => {
-  const files = await getDirectory(directory);
+  const files = await readdir(directory);
   files.forEach(async (file) => {
     const filePath = path.join(directory, file);
-    const stat = await getFileStat(filePath);
-    /** 만약 dir이면 차례대로 search. 이전 dir의 search가 끝날 때 까지 기다리기 */
+    const stat = await stat(filePath);
+    /** 만약 dir이면 차례5f대로 search. 이전 dir의 search가 끝날 때 까지 기다리기 */
     if (stat.isDirectory()) await searchDirectory(filePath);
     /** path의 확장자가 .js 인 경우 출력 */ else if (
       path.extname(filePath) === ".js"
